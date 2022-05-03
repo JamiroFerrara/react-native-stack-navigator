0159db01b60a6f20a8d1acb2a78d3382324720e6
Jamie <83338941+JamiroFerrara@users.noreply.github.com>
2022-05-03 13:59:45 +0200

    Update

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Easing} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createStackNavigator, TransitionPresets, CardStyleInterpolators } from '@react-navigation/stack';
 
 import style from './style.module.scss';
 import { TouchableHighlight } from 'react-native';
@@ -27,12 +27,39 @@ function DetailsScreen({navigation}) {
   );
 }
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
+
+const openConfig = {
+  animation: 'spring',
+  config: {
+    stiffness: 1000,
+    damping: 50,
+    mass: 3,
+    overshootClamping: false,
+    restDisplacementThreshold: 0.01,
+    restSpeedThreshold: 0.01,
+  },
+}
+
+const closeConfig = {
+  animation: 'timing',
+  config: {
+    duration: 100,
+    easing:Easing.linear
+  }
+}
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Home" 
+          screenOptions={{
+            headerTitleAlign: "center", 
+            gestureEnabled: true, 
+            gestureDirection: "horizontal",
+            cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+            }} 
+            headerMode="float">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
